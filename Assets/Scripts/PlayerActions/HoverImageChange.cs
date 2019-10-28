fc45edc5c7b9154801d3ce7ef90de962e5e245a6
silvai <ella.silva@gatech.edu>
2019-10-27 20:01:14 -0400

    added scenes to build scripts

--- Assets/Scripts/PlayerActions/HoverImageChange.cs
+++ Assets/Scripts/PlayerActions/HoverImageChange.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using UnityEditor.SceneManagement;
 
 public class HoverImageChange : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
 {
